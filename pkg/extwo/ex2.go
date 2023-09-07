33f589b2223eae7719cd89226a6af80ab435b6a7
sentriz <senan@senan.xyz>
2023-09-07 10:36:17 +0100

    add row and status

--- pkg/extwo/ex2.go
+++ pkg/extwo/ex2.go
@@ -6,3 +6,18 @@ package extwo
 	Product wants two new features to be delivered: filtering and sorting on those statuses, we also need to keep supporting pagination.
 	How can we approach the problem? Which data structure do we need?
 */
+
+type Status string
+
+const (
+	StatusSubmitted  Status = "submitted"
+	StatusExpired    Status = "expired"
+	StatusInProgress Status = "in-progress"
+	StatusDisabled   Status = "disabled"
+)
+
+type Row struct {
+	SubmittedAt int
+	ExpiredAt   int
+	IsActive    bool
+}
