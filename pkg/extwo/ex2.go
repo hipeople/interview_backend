796df7e575c9cd768c28277dc1edf14563cc22d6
sentriz <senan@senan.xyz>
2023-10-18 13:35:12 +0100

    ​

--- pkg/extwo/ex2.go
+++ pkg/extwo/ex2.go
@@ -1,7 +1,7 @@
 package extwo
 
 /*
-	Products wants to start scraping websistes for informations about job roles, and skills requestsd to build a better test library.
+	Product wants to start scraping websistes for informations about job roles, and skills requestsd to build a better test library.
  	Engineering is asked to come up with a first implementeation that can receive urls and store the resutls in a database for furure analysis.
   	The system should be able to work in parallel, handling errors from the scraping process gracefully.
 */
